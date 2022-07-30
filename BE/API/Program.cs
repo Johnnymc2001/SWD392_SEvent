96d3d01a98508a38b07bb914dfc0d88ffb87b20c
JohnnyMC <johnnymc2001@gmail.com>
2022-07-30 12:20:04 +0700

    Init

---
+++ BE/API/Program.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Application.Core;
+using Domain;
+using FirebaseAdmin;
+using Google.Apis.Auth.OAuth2;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API
+{
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			var host = CreateHostBuilder(args).Build();
+			using var scope = host.Services.CreateScope();
+			var services = scope.ServiceProvider;
+
+			try
+			{
+				var context = services.GetRequiredService<DataContext>();
+				var userManager = services.GetRequiredService<UserManager<User>>();
+				var config = services.GetRequiredService<IConfiguration>();
+				var rm = services.GetRequiredService<RoleManager<IdentityRole<Guid>>>();
+
+				FirebaseApp.Create(new AppOptions()
+				{
+					Credential = GoogleCredential.FromJson("Insert Key Here")
+				});
+
+
+				if (await rm.Roles.CountAsync() == 0)
+				{
+					await rm.CreateAsync(new IdentityRole<Guid>("User"));
+					await rm.CreateAsync(new IdentityRole<Guid>("Admin"));
+				}
+
+				// await context.Database.MigrateAsync();
+				// await Seed.SeedData(context, userManager);
+				// await Seed.ClearSeedData(context, userManager);
+			}
+			catch (Exception ex)
+			{
+				var logger = services.GetRequiredService<ILogger<Program>>();
+				logger.LogError(ex, "An error occured during migration;");
+			}
+
+
+			host.Run();
+		}
+
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+
+				});
+	}
+}
